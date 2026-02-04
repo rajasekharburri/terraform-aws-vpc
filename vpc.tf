cb05e3bfc4d6536a4ab12bf03139f24603c24ac3
rajasekhar burri <burriraja@gmail.com>
2026-02-04 23:46:25 +0530

    msg

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
- # VPC
+# VPC
 resource "aws_vpc" "main" {
-  cidr_block       = var.vpc_cidr  #"10.0.0.0/16"
+  cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
   enable_dns_hostnames = true
 
