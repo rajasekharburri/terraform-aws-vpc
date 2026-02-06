2dc81e7e6c7d20fce4c145a05785ce556ceb2e49
rajasekhar burri <burriraja@gmail.com>
2026-02-06 15:43:41 +0530

    terraform

--- locals.tf
+++ locals.tf
@@ -7,7 +7,7 @@ locals{
         Terraform = true
     }
     common_name_suffix = "${var.project_name}-${var.environment}" # roboshop-dev
-    # az_names = slice(data.aws_availability_zones.available.names, 0, 2 )
+    az_names = slice(data.aws_availability_zones.available.names, 0, 2 )
 }
 
 #locals cant overide
